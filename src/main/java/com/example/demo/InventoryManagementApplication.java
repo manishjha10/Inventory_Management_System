252a815d255e2e299d3bf8d82afa4c2072cbcd8b
Rahil <rahilmohd255@gmail.com>
2026-05-05 15:07:22 +0530

    Added security config and dashboard API

--- src/main/java/com/example/demo/InventoryManagementApplication.java
+++ src/main/java/com/example/demo/InventoryManagementApplication.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.example.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class InventoryManagementApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(InventoryManagementApplication.class, args);
 	}
-
 }
