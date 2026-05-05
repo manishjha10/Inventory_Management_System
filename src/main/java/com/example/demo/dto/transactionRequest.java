3aedbadf47c3b947c1802aa068a81b500152feb8
Laxita-13 <samantanilaxita11@gmail.com>
2026-05-05 14:34:02 +0530

    Added transaction features

--- src/main/java/com/example/demo/dto/transactionRequest.java
+++ src/main/java/com/example/demo/dto/transactionRequest.java
@@ -1,1 +1,10 @@
+package com.ims.dto;
 
+import lombok.Data;
+
+@Data
+public class TransactionRequest {
+    private Long productId;
+    private Integer quantity;
+    private Long userId;
+}
--- src/main/java/com/example/demo/service/transactionService.java
+++ src/main/java/com/example/demo/service/transactionService.java
@@ -1,1 +1,45 @@
+package com.ims.service;
 
+import com.ims.entity.Transaction;
+import com.ims.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+
+    private final TransactionRepository transactionRepo;
+
+    // STOCK IN
+    public Transaction stockIn(Long productId, int qty, Long userId) {
+        Transaction t = new Transaction();
+        t.setProductId(productId);
+        t.setQuantity(qty);
+        t.setType("IN");
+        t.setTimestamp(LocalDateTime.now());
+        t.setUserId(userId);
+
+        return transactionRepo.save(t);
+    }
+
+    // STOCK OUT
+    public Transaction stockOut(Long productId, int qty, Long userId) {
+        Transaction t = new Transaction();
+        t.setProductId(productId);
+        t.setQuantity(qty);
+        t.setType("OUT");
+        t.setTimestamp(LocalDateTime.now());
+        t.setUserId(userId);
+
+        return transactionRepo.save(t);
+    }
+
+    // GET HISTORY
+    public List<Transaction> getAllTransactions() {
+        return transactionRepo.findAll();
+    }
+}
